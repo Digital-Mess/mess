c44060007dd791e4b963f1a74dbc8e4c4fefca2c
ReshmaSKumar <gs_reji@yahoo.co.in>
2018-02-06 14:28:16 +0530

    New File Organisation

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+
+<?php include 'includes/header.php' ?>
+
+<body>
+
+<?php include 'includes/nav.php' ?>
+
+<div style="padding: 5% 12%; text-align: center; background: url('images/indexImage.jpg');">
+	<div style="height: 200px;">
+	</div>
+	<div class="row" style="margin: 4% 0;">
+		<div class="col-md-3 IndexCard">
+			<img src="images/food.png" style="width: 100px; height: 100px;">
+			<h3>Mess Menu</h3>
+			<p>Explore our range of mouth-watering Pizza's, delivered hot and fresh in 30 minutes!</p>
+			<button class="btn btn-IndexCard btn-md">Discover Menu</button>
+		</div>
+		<div class="col-md-3 IndexCard">
+			<img src="images/shop.png" style="width: 100px; height: 100px;">
+			<h3>Find Near Me</h3>
+			<p>Find Restrurents and more near you</p>
+			<br>
+			<button class="btn btn-IndexCard btn-md">Discover Menu</button>
+		</div>
+		<div class="col-md-3 IndexCard">
+			<img src="images/hostel.png" style="width: 100px; height: 100px;">
+			<h3>Hostels</h3>
+			<p>Lorem Ipsum has Got a hihgh value than me</p>
+			<br>
+			<button class="btn btn-IndexCard btn-md">Discover Menu</button>
+		</div>
+		<div class="col-md-3 IndexCard">
+			<img src="images/bill.png" style="width: 100px; height: 100px;">
+			<h3>Bill Payments</h3>
+			<p>Make your Mess bill payments online</p>
+			<br>
+			<button class="btn btn-IndexCard btn-md">Discover Menu</button>
+		</div>
+	</div>
+</div>
+
+
+<div style="padding: 5% 12%; padding-bottom: 1%; background: #FF8000;position: relative;">
+	<div class="row">
+			<div class="col-md-6">
+				<h1>Choose your Mess</h1>
+				<!-- <h4 style="margin-top: 12px; max-width: 85%; line-height: 25px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, </h4> -->
+			</div>
+				<div class="col-md-6">
+			</div>		
+	</div>
+	<div class="row" style="margin: 4% 0;">
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>ABH</h3>
+				</div>
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>HJB</h3>
+				</div>
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>VVS</h3>
+				</div>
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>CVR</h3>
+				</div>
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>SNH</h3>
+				</div>
+				<div class="col-md-2 IndexCard-small">
+					<img src="images/shop.png" style="width: 100px; height: 100px;">
+					<h3>Hostels</h3>
+				</div>
+	</div>
+	<!-- <img src="images/spoon.png" style="position: absolute;
+				    right: 0;
+				    opacity: 0.3;s
+					webkit-transform: rotate(0deg);
+ 			         transform: rotate(0deg);opacity: 0.3;margin-top: -5rem;
+ 			         margin-right: 10rem;
+					">
+				<img src="images/fork.png" style="position: absolute;
+							    right: 0;
+							    opacity: 0.3;
+								webkit-transform: rotate(134deg);
+				             	transform: rotate(90deg);
+							    opacity: 0.3;
+							    margin-right:-6rem;
+							    width: 525.42px
+				">
+			 -->
+</div>
+
+<div style="padding: 5% 12%; background: #577783; z-index: 2; position: relative;">
+	<h1 style="">Lorem Ipsum</h1>
+	<div class="row" style="margin-top: 2%;">
+		<div class="col-md-7">
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+			</p>
+		</div>
+		<div class="col-md-offset-1 col-md-4">
+			<img class="img-rounded col-md-4" style="width: 100%;"  src="https://images.unsplash.com/photo-1428550443830-190057dc8098?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=04d9ec6cb1a9a4f259e0f96894dd71d3&auto=format&fit=crop&w=1951&q=80">
+		</div>
+	</div>	
+</div>
+
+
+
+<div style="padding: 5% 12%; background: #388D80;">
+	<div style="max-width: 80%;margin-top: 4%;">
+			<div class="panel panel-info">
+				  <div class="panel-heading">Panel heading without title</div>
+				  <div class="panel-body">
+				   	One big advantage of using the hosted jQuery from Google or Microsoft:
+					Many users already have downloaded jQuery from Google or Microsoft when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.
+				  </div>
+			</div>
+			<div class="panel panel-info">
+				  <div class="panel-heading">Panel heading without title</div>
+				  <div class="panel-body">
+				   	One big advantage of using the hosted jQuery from Google or Microsoft:
+					Many users already have downloaded jQuery from Google or Microsoft when visiting another site. As a result, it will be loaded from cache when they visit your site, which leads to faster loading time. Also, most CDN's will make sure that once a user requests a file from it, it will be served from the server closest to them, which also leads to faster loading time.
+				  </div>
+			</div>
+
+	</div>
+</div>
+
+	<?php include 'includes/footer.php' ?>
+
+</body>
+</html>
